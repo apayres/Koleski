3173798a81136f4c75dea309adf4fedde05030e6
apayres <adampayres@gmail.com>
2019-06-10 20:03:32 -0400

    Testing and stuff

---
+++ Koleski.IntegrationTesting/KoleskiTestWrapper.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using System.Transactions;
+
+namespace Koleski.IntegrationTesting
+{
+    [TestFixture]   
+    public abstract class KoleskiTestWrapper
+    {
+        private TransactionScope _transaction;
+
+        [SetUp]
+        public void Setup()
+        {
+            _transaction = new TransactionScope(TransactionScopeOption.Required);
+        }
+
+        [TearDown]
+        public void Teardown()
+        {
+            _transaction.Dispose();
+        }
+    }
+}
